212596c22f04410e513d8930756c657dc79bacca
14myclass <just4raki@hotmail.com>
2018-04-11 21:28:57 -0500

    added basic text to push to  project repo

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Attempt to set up project");
+            Console.ReadLine();
+        }
+    }
+   
+}
