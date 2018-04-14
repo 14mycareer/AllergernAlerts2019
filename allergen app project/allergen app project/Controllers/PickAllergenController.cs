c19df9ae3f53bd5cd179a7d169f57d324f77599d
14myclass <just4raki@hotmail.com>
2018-04-14 01:40:43 -0500

    created pick allergen home page display

---
+++ allergen app project/allergen app project/Controllers/PickAllergenController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace allergen app project.Controllers
+{
+    public class PickAllergenController : Controller
+    {
+        // GET: /<controller>/
+
+        //Action method for displaying "Pick Allergen" page 
+        public IActionResult Index()
+        {
+
+        //Retrieve allergens for a drop down list
+        var allergens = GetAllAllergens();
+
+        var model = new PickAllergen();
+
+
+        //Make a list of SelectListItems to be rendered on the page
+        model.Allergens = GetSelectListItem(allergens);
+            return View(model);
+        }
+    }
+}
