3e223151581711ef5e85c3525cec988a28d6d643
14myclass <just4raki@hotmail.com>
2018-04-14 00:52:11 -0500

    add model for drop down list of allergens

---
+++ allergen app project/allergen app project/Models/PickAllergen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace allergen app project.Models
+{
+    public class PickAllergen
+    {
+    //This property will reserve space for the allergen selected by the usser
+    [Required]
+    [Display(Name="Allergen")]
+    public string Allergen { get; set; }
+
+    //This property will hold the allergens available for the user to choose
+    public IEnumerable<SelectListItem> Allergens { get; set; }
+    }
+}
