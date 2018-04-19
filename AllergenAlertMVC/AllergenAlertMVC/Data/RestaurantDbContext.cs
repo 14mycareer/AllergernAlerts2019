76f484f8f6e5b4abaec60ed8e377a89e6d15c43d
14myclass <just4raki@hotmail.com>
2018-04-19 07:57:45 -0500

    initial project commit. added dbcontext

---
+++ AllergenAlertMVC/AllergenAlertMVC/Data/RestaurantDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllergenAlertMVC.Data
+{
+    public class RestaurantDbContext : DbContext
+
+    {
+        public DbSet<Restaurant>Restaurants { get; set; }
+        public RestaurantDbContext(DbContextOptions<RestaurantDbContext>options)
+            :base(options)
+        {
+
+        }
+    }
+}
