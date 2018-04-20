dab4cbb0711c02f93c5bdaf30aeba75443ab98ea
14myclass <just4raki@hotmail.com>
2018-04-20 14:58:11 -0500

    created restaurant controller w/home page view

---
+++ AllergenAlertMVC/AllergenAlertMVC/Controllers/RestaurantController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AllergenAlertMVC.Controllers
+{
+    public class RestaurantController : Controller
+    {
+
+        //define database to which to attach restaurant objects
+        private RestaurantDbContext context;
+
+        //database constructor
+        public RestaurantController(RestaurantDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            //need to fix to display listing of restaurants; change c=>c.Category
+            //IList<Restaurant> restaurants = context.Restaurants.Include(c => c.Category).ToList();
+
+            IList<Restaurant> restaurants = context.Restaurants.ToList();
+
+            return View(restaurants);
+        }
+    }
+       
+}
